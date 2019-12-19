cd6d663d99a4e2045a921d9ad88871bb8919da96
Mooje Kong <mooje.kong@weather.com>
2019-12-19 16:17:29 +0900

    update

--- 000000/vagrant/hyperledger_besu/Getting_started/configIBFT.sh
+++ 000000/vagrant/hyperledger_besu/Getting_started/configIBFT.sh
@@ -10,6 +10,15 @@ mkdir -p IBFT-Network/Node-4/data
 cd IBFT-Network
 wget https://raw.githubusercontent.com/hlkug/meetup/master/000000/vagrant/hyperledger_besu/Getting_started/ibftConfigFile.json
 
-./besu/bin/besu operator generate-blockchain-config --config-file=ibftConfigFile.json --to=networkFiles --private-key-file-name=key
+~/besu/bin/besu operator generate-blockchain-config --config-file=ibftConfigFile.json --to=networkFiles --private-key-file-name=key
 
 cp ./networkFiles/genesis.json .
+
+
+cd
+mkdir scripts
+cd scripts
+wget https://raw.githubusercontent.com/hlkug/meetup/master/000000/vagrant/hyperledger_besu/Getting_started/startNode1.sh
+wget https://raw.githubusercontent.com/hlkug/meetup/master/000000/vagrant/hyperledger_besu/Getting_started/startNode2.sh
+wget https://raw.githubusercontent.com/hlkug/meetup/master/000000/vagrant/hyperledger_besu/Getting_started/startNode3.sh
+wget https://raw.githubusercontent.com/hlkug/meetup/master/000000/vagrant/hyperledger_besu/Getting_started/startNode4.sh
